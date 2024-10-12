6a97273b88bd6bdc99ef7d1f42fdbe340ba75898
jonathan salim <jonathansalim003@gmail.com>
2024-10-12 11:31:49 +0700

    init

--- routes/web.php
+++ routes/web.php
@@ -5,7 +5,6 @@ use App\Http\Controllers\ProductController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
 Route::resource('products', ProductController::class);
 Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard');
 });
